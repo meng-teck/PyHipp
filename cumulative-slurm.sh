9cd54162a7f153b63bb83a85c9fcd5cd4fe8d07c
AWS ParallelCluster user <ec2-user@ip-10-0-14-195.ap-southeast-1.compute.internal>
2025-09-29 04:38:14 +0000

    Quiz 1B

--- cumulative-slurm.sh
+++ cumulative-slurm.sh
@@ -15,5 +15,8 @@
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
 import DataProcessingTools as DPT; \
-lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
-lfall.save();"
+import os; \
+alldirs = [d for d in os.listdir('.') if os.path.isdir(d)]; \
+exclude_dirs = [d for d in alldirs if d not in ['20181101', '20181102']]; \
+wfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*', '*array04*'] + exclude_dirs, objtype=pyh.Waveform); \
+wfall.save();"
